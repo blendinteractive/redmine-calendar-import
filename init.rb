0ad6d4baa697d5a0f242649e0b905ca9f9f6b3bb
sclarson <seth.larson@gmail.com>
2012-09-21 15:39:16 -0500

    Updates author and adds Dispatcher for redmine 1.4

--- init.rb
+++ init.rb
@@ -1,17 +1,24 @@
 # Empty redmine plguin
 require 'redmine'
-
 # TODO: Change this to use the name of your plugin
 RAILS_DEFAULT_LOGGER.info 'Starting Calendar Import plugin for RedMine'
 
 # TODO: Change the name 
 Redmine::Plugin.register :calendar_import do
   name 'Calendar Import'
-  author 'Chris Lukenbill'
+  author 'Blend Interactive'
   description 'This to allow users to create time entries and issues directly from the calendar system they are using.'
   version '0.1.0'
 
   menu(:top_menu, :calendar_imports, {:controller => "calendar_imports", :action => 'index'}, :caption => 'Calendar Import', :if => Proc.new{ User.current.logged? })
 end
 
+require 'dispatcher'
+Dispatcher.to_prepare :calendar_import_plugin do
+  require_dependency 'principal'
+  require_dependency 'user'
+  require_dependency 'project'
+  User.send(:include,UserCalendarsPatch)
+  Project.send(:include,ProjectPatch)
+end
 
