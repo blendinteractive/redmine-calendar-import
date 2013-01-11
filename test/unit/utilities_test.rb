66e9407f4078f29103049bdb1a1f98a4f9d48bf4
seth larson <seth.larson@gmail.com>
2013-01-11 11:36:48 -0600

    Test the times it shouldn't change as well

--- test/unit/utilities_test.rb
+++ test/unit/utilities_test.rb
@@ -2,6 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '../../test_helper.rb')
 require File.expand_path(File.dirname(__FILE__) + '../../../script/utilities.rb')
 
 class UtilitiesTest < ActiveSupport::TestCase
+  test "0 Minutes Rounds to 0" do
+    assert_equal 0, round_to_15_minutes(0)
+  end
+
+  test "120 Minutes Rounds to 120" do
+    assert_equal 120, round_to_15_minutes(120)
+  end
+
   test "1 Minute Rounds to 15" do
     assert_equal 15, round_to_15_minutes(1)
   end
